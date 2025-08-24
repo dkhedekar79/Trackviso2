06372fcb8b896452aa812ba4dddc881b5c5deca3
dkhedekar79 <57810277+dkhedekar79@users.noreply.github.com>
2025-08-24 11:17:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
 
-              <OnboardingModal />
+              
 
               {/* Protected Routes */}
               <Route path="/dashboard" element={
