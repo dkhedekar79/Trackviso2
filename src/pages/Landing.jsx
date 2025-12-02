0437bf56c75e054f9b48610829f5074ad4be08f0
dkhedekar79 <57810277+dkhedekar79@users.noreply.github.com>
2025-12-02 21:05:45 +0000

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -71,7 +71,7 @@ const Landing = () => {
           <span className="bg-gradient-to-r from-purple-400 via-pink-400 to-purple-600 bg-clip-text text-transparent">
             AI
           </span>{" "}
-          in the world's best revision system
+          in your hands.
         </>
       ),
       subtitle:
