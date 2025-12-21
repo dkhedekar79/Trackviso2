dd13e36f2c622b5cc436c25e718e020eaf349eb0
Daiwik Khedekar <dskhedekar7@gmail.com>
2025-12-21 12:55:31 +0000

    fix: import useRef in GamificationContext to resolve ReferenceError

--- src/context/GamificationContext.jsx
+++ src/context/GamificationContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState, useEffect, useRef } from "react";
 import logger from '../utils/logger';
 import { APP_CONFIG } from '../constants/appConfig';
 
